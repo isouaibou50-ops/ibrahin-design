72afa29e2d40f686c1853d3abfb58c0b0b3f82cc
isouaibou50 <isouaibou50@gmail.com>
2025-10-24 09:20:12 -0700

    fIX bugs User model

--- models/User.js
+++ models/User.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    _id : { type: String, required: true},
-    name: { type: String, required: true},
-    email: {type : String, required: true},
-    imageUrl: {type: String, require: true},
-    cartItems: { type: Object, default: {}}
-}, { minimize: false})
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
 
-const User = model.models.user || mongoose.model('user', userSchema);
+const User = mongoose.models.user || mongoose.model("user", userSchema);
 
 export default User;
