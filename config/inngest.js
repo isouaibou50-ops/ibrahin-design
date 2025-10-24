afc0edcc8abd3326bebdf9a51cb2bda6dbd03197
isouaibou50 <isouaibou50@gmail.com>
2025-10-24 08:34:17 -0700

    Add and Setup Ingest and Clerk webhook

---
+++ config/inngest.js
@@ -0,0 +1,58 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "ibrahimdesign-app" });
+
+// Inngest function to save data to adatabase
+export const syncUserCreation = inngest.createFunction(
+    {
+        id: 'sync-user-from-clerk'
+    },
+    { event: 'clerk/user.created'},
+    async ({event}) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            imageUrl: image_url
+        }
+        await connectDB();
+        await User.create(userData)
+    }
+)
+
+// Inngest function to update data to adatabase
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id: 'update-user-with-clerk'
+    },
+    { event: 'clerk/user.updated'},
+    async ({event}) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            imageUrl: image_url
+        }
+        await connectDB();
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
+
+// Inngest function to delete data to adatabase
+export const syncUserDeletion = inngest.createFunction(
+    {
+        id: 'delete-user-with-clerk'
+    },
+    { event: 'clerk/user.deleted'},
+    async ({event}) => {
+        const { id } = event.data;
+        
+        await connectDB();
+        await User.findByIdAndDelete(id)
+    }
+)
