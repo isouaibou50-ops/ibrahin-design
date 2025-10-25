5906610949a9741c7568d08940638db2fea15cb0
isouaibou50 <isouaibou50@gmail.com>
2025-10-25 21:44:59 +0200

    Fix ingest function

--- config/inngest.js
+++ config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import connectDB from "./db";
 import User from "@/models/User";
+import Order from "@/models/Order";
 
 // Create the Inngest client
 export const inngest = new Inngest({ id: "ibrahimdesign-app" });
@@ -84,6 +85,28 @@ export const syncUserDeletion = inngest.createFunction(
   }
 );
 
+
+
+// Inngest Function to create user's order in database
+export const createUserOrder = inngest.createFunction(
+  { id: "create-user-order", batchEvents: { maxSize: 5, timeout: '5s'} },
+  { event: "order/created" },
+  async ({ events }) => {
+    const orders = events.map((event) => {
+      return {
+        userId: event.data.userId,
+        items: event.data.items,
+        amount: event.data.amount,
+        address: event.data.address,
+        date: event.data.date
+      }
+    })
+
+    await connectDB();
+    await Order.insertMany(orders)
+    return { succes: true, processed: orders.length}
+  }
+);
 
 
 
