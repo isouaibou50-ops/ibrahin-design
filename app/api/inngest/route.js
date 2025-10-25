9d3ae86587b1814cb0c42d177b191602400225d4
isouaibou50 <isouaibou50@gmail.com>
2025-10-25 20:25:55 +0200

    Fix bugs with inngest batch size

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -20,7 +20,7 @@ export const createUserOrder = inngest.createFunction(
   {
     id: 'create-user-order',
     batchEvents: {
-      maxSize: 25,
+      maxSize: 5,
       timeout: '5s'
     }
   },
