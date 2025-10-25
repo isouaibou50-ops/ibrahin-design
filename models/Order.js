97887a320f06c1e4f537e54b7b416eca63a5e5f4
isouaibou50 <isouaibou50@gmail.com>
2025-10-25 20:31:48 +0200

    Fix bugs with inngest batch size

--- models/Order.js
+++ models/Order.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const orderSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true, ref: "user" },
-    items: [{ product: {String, required: true, ref: 'product'}, quantity: {type: Number, required: true} }],
+    items: [{ product: {type: String, required: true, ref: 'product'}, quantity: {type: Number, required: true} }],
     amount: { type: String, required: true },
     address: { type: String, ref: 'address', required: true },
     status: { type: String, required: true, default: 'Order Placed' },
