34a741f63ff9268eb80f5536ccea91532bc4bb45
isouaibou50 <isouaibou50@gmail.com>
2025-10-25 20:56:30 +0200

    Fix bugs with inngest batch size

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,7 +1,5 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
-import connectDB from "@/config/db";
-import Order from "@/models/Order";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
@@ -18,28 +16,25 @@ export const { GET, POST, PUT } = serve({
 // Inngest Function to create user's order in database
 export const createUserOrder = inngest.createFunction(
   {
-    id: 'create-user-order',
+    id: "create-user-order",
     batchEvents: {
       maxSize: 5,
-      timeout: '5s'
-    }
+      timeout: "5s",
+    },
   },
-  {event: 'order/created'},
-  async ({events}) => {
-    const orders = events.map((event) => {
-      return {
-        userId: event.data.userId,
-        items: event.data.items,
-        amount: event.data.amount,
-        address: event.data.address,
-        date: event.data.data
-      }
-    })
+  { event: "order/created" },
+  async ({ events }) => {
+    const orders = events.map((event) => ({
+      userId: event.data.userId,
+      items: event.data.items,
+      amount: event.data.amount,
+      address: event.data.address,
+      date: event.data.date,
+    }));
 
-    await connectDB()
-    await Order.insertMany(orders)
+    await connectDB();
+    await Order.insertMany(orders);
 
-    return { success: true, processed: orders.length}
+    return { success: true, processed: orders.length };
   }
-
-)
+);
