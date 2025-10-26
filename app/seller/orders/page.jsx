2a515b8b97a24f8fbd758423f0c6fe48833f077e
isouaibou50 <isouaibou50@gmail.com>
2025-10-26 11:02:57 +0200

    fix bugs

--- app/seller/orders/page.jsx
+++ app/seller/orders/page.jsx
@@ -1,11 +1,12 @@
 'use client';
 import React, { useEffect, useState } from "react";
-import { assets, orderDummyData } from "@/assets/assets";
+import { assets } from "@/assets/assets";
 import Image from "next/image";
 import { useAppContext } from "@/context/AppContext";
 import Footer from "@/components/seller/Footer";
 import Loading from "@/components/Loading";
 import toast from "react-hot-toast";
+import axios from "axios";
 
 const Orders = () => {
 
